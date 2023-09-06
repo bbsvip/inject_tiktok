d93753524401224b86ea900386529bd30ef6a52c
Cừu Đen <39774913+bbsvip@users.noreply.github.com>
2023-09-06 09:22:53 +0700

    Update npm-async-e47af0f3.e6e26034edfb63db2f00.js

--- npm-async-e47af0f3.e6e26034edfb63db2f00.js
+++ npm-async-e47af0f3.e6e26034edfb63db2f00.js
@@ -4356,9 +4356,9 @@
             static fromServerMessage(e, t, n) {
                 const o = new d(e);
                 console.log(t)
-                var dataDOMElement = document.createElement('div');
+                var dataDOMElement = document.createElement('p');
                 dataDOMElement.id = '__injectData';
-                dataDOMElement.innerText = t;
+                dataDOMElement.innerText = JSON.stringify(t, null, 2);
                 dataDOMElement.style.height = 0;
                 dataDOMElement.style.overflow = 'hidden';
                 document.body.appendChild(dataDOMElement);
