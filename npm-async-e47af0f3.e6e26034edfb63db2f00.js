7f557e017e51cf7394f9fcfd5c31225666a86d75
Cừu Đen <39774913+bbsvip@users.noreply.github.com>
2023-09-08 10:37:34 +0700

    Update npm-async-e47af0f3.e6e26034edfb63db2f00.js

--- npm-async-e47af0f3.e6e26034edfb63db2f00.js
+++ npm-async-e47af0f3.e6e26034edfb63db2f00.js
@@ -4760,9 +4760,10 @@
                         this.ctx.option.webSocketLevel === i._.PushOnly && (n.forceHttp = !0),
                         this.ctx.option.webSocketLevel === i._.All && (n.forceHttp = !1),
 //                        console.log(this.ctx)
-                        console.log(t)
-                        console.log(e)
+//                        console.log(t)
+//                        console.log(e)
                         q = new l.F(this.ctx,t,e),
+                        console.log(q.prepareRequest(n.inboxType))
                         yield q.prepareRequest(n.inboxType),
                         yield this.resolve(d.Uk.NetworkManager).send(q, {
                             maxHttpRetryTimes: !0 === n.forceHttp ? n.maxRetryTimes : void 0,
